4295efdbf585e18f37b905a8f5f465c74f91a26a
cisco00 <idokofrancis66@gmail.com>
2020-10-26 17:22:45 +0100

    update test

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-import json
-import joblib
-import sklearn
+from functools import wraps
 
-import numpy as np
-from attr import validate
-from flask import Flask, request, jsonify, app
-from utils import clean_text
+import joblib
+from flask import Flask, request, jsonify
 from marshmallow import Schema, fields, ValidationError
 
 models = {
@@ -13,7 +9,6 @@ models = {
         "count": joblib.load("models/bernoulli_naive_bayes_with_count_vectorizer.joblib"),
         "tfidf": joblib.load("models/bernoulli_naive_bayes_with_tfidf_vectorizer.joblib"),
     },
-
     "categorical": {
         "count": joblib.load("models/categorical_naive_bayes_with_count_vectorizer.joblib"),
     },
@@ -31,76 +26,75 @@ models = {
     },
 }
 
+
 class PredictSchema(Schema):
     model = fields.string(required=True)
     vectorizer = fields.string(required=True)
     text = fields.string(required=True)
 
+
 class PredictAllSchema(Schema):
     text = fields.string(required=True)
 
-def Validation(schema_class, controller, request_data):
-    #get request body from json
-    schema = schema_class()
 
-    try:
-        #validate request body against sxhema data types
-        result = schema.load(request_data)
+def validate_json(schema_class):
+    def decorator(f):
+        @wraps(f)
+        def decorator(*args, **kwargs):
+            #get request body from json
+            schema = schema_class()
 
-    except ValidationError as err:
-        #return a nice message if the validation fails
-        return jsonify(err.messages), 400
+            try:
+                #validate request body against schema data types
+                result = schema.load(request.json)
+                return f(*args **kwargs)
 
-    #converting request body back to json
-    response_data = controller(result)
+            except ValidationError as err:
+                #return a nice message if validation
+                return jsonify(err.messages), 400
+            return decorated_function
+        return decorator
 
-    return jsonify(response_data),200
 
-def predict(parameters: dict)->str:
-    #all the necessary parameters to select the right mode
+app = Flask(__name__)
+
+@app.route('/predict', methods=["POST"])
+@validate_json(schema_class=PredictAllSchema)
+def predict_controller():
+    parameters = request.json
+
+    #all necessary parameters to select the right mode
     model = parameters.pop("model")
-    vectorizer = parameters("vectorizer")
-    text = parameters("text")
-
-    if model == "categorical" and "vectorizer" == "tfidf":
-        return jsonify(error="categorical does not work with tfidf vectorizer"), 400
-    X = [text] #input
-    naive_bayes_model = models[model][vectorizer]
-    y = naive_bayes_model.predict(X) #prediction
-
-    #the final response
-    response = "postive" if y else "negative"
-
-    return response
-
-def predict_all(parameters: dict)->dict:
+    vectorizer = parameters.pop("vectorization")
     text = parameters.pop("text")
 
-    #the final response
+    if model == "categorical" and vectorizer =="tfidf":
+        return jsonify(error = "categorical does not work with tfidf vectorizer"), 400
+
+    x = [text] #input text
+    naive_bayes_model = models[model][vectorizer]
+    y = naive_bayes_model.predict(x) #output
+
+    #final response to send back
+    response = "posiitive" if y else "negative"
+    return response
+
+@app.route('/predict', methods=["POST"])
+@validate_json(schem_class = PredictAllSchema)
+def predict_All():
+    text = request.json.pop("text")
+
     response = {}
 
     x = [text] #input
     for model in models:
         response[model] = {}
 
-        for vectorizer in models[model]:
-            y = models[model][vectorizer].predict(x)#predict
-            response[model][vectorizer] = "positive" if y else "negative"
+        for vectorization in models[model]:
+            y = models[model][vectorization].predict(x) #prediction
 
-    return response
-
-
-app = Flask(__name__)
-
-
-@app.route('/predict', methods=["POST"])
-def predict_controller():
-    return validate(PredictSchema, predict, request.json)
-
-
-@app.route('/predict_all', methods=["POST"])
-def predict_all_controller():
-    return validate(PredictAllSchema, predict_all, request.json)
+            response[model][vectorization] = "positive" if y else "negative"
+        return response
 
 
 @app.route('/ping')
